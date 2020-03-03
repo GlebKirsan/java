5b789130d2718be83361a8aa3ea993f474b379f0
glkv <glkv@protonmail.ch>
2020-03-04 00:18:40 +0300

    a

---
+++ stepik/2_module/precision/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static boolean doubleExpression(double a, double b, double c) {
+        return Math.abs(a + b - c) <= 0.0001;
+    }
+    
+	public static void main(String[] args) {
+		System.out.println(doubleExpression(0.1, 0.2, 0.3));
+	}
+}
