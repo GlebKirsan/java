59c0883ee4358b2b7b6d6e51f4f3180719d6ee4d
GlebKirsan <glkv@protonmail.ch>
2020-03-03 19:47:35 +0300

    Comments was added

--- stepik/1_week/hello_world/HelloWorld.java
+++ stepik/1_week/hello_world/HelloWorld.java
@@ -1,4 +1,15 @@
+/**
+ * A program that prints <code>Hello, world!</code>
+ *
+ * @author Gleb Kirsanov
+ * @version 1.0
+ */
 public class HelloWorld {
+	/**
+	 * Program entry point
+	 *
+	 * @param args command-line arguments
+	 */
 	public static void main(String[] args) {
 		System.out.println("Hello, world!");
 	}
